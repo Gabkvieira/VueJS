96baafb897dc00120a2608df53f1b8962244159a
Gabriel Vieira <gabkv99@hotmail.com>
2024-10-14 09:42:04 -0300

    alterando código: aula eventos para manipular dados

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { reactive } from 'vue';
+
 const nome = "Gabriel Vieira";
 const meuObj = {
   nome:"Gabriel",
@@ -19,6 +21,24 @@ const gostaDoSuperman = true;
 
 const estaAutorizado = true;
 
+// let contador = 0;
+const estado = reactive({
+  contador: 0,
+  email: '',
+})
+
+function incrementar() {
+  estado.contador++;
+}
+
+function decrementar() {
+  estado.contador--;
+}
+
+function alteraEmail(evento) {
+  estado.email = evento.target.value;
+}
+
 </script>
 
 
@@ -32,6 +52,22 @@ const estaAutorizado = true;
   <h1 v-else>Não possui acesso</h1>
 
   <button :disabled="botaoEstaDesabilitado">Enviar mensagem</button>
+
+  <br />
+  <hr />
+
+{{ estado.contador }}
+
+<button @click="incrementar" type="button">+</button>
+<button @click="decrementar" type="button">-</button>
+
+<br />
+<hr />
+
+{{ estado.email }}
+<!-- <input type="email" @keyup="evento => estado.email = evento.target.value"> -->
+<input type="email" @keyup="alteraEmail">
+
 </template>
 
 <style scoped>
